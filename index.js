60342bd794f0139686c84a2aaf5214c50ac548a1
Subhajit Sahu <wolfram77@gmail.com>
2018-06-07 14:02:34 +0530

    :snowman: update .round(), becuase it did not work for a few cases in ifct2017bot

--- index.js
+++ index.js
@@ -114,8 +114,8 @@ function isWhole(n) {
 };
 // 18. number-roundto (roundTo)
 function roundTo(val, pre=1e-12) {
-  val = Math.round(val/pre)*pre;
-  return Math.round(val*1e+12)*1e-12;
+  var val = Math.round(val/pre)*pre;
+  return Math.round(val*1e+12)/1e+12;
 };
 // 19. number-toindiansystem (toIndianSystem)
 function toIndianSystem(n, sep) {
