a21b21691a36fc164e53320902cb7435fa01e379
Subhajit Sahu <wolfram77@gmail.com>
2018-12-22 17:20:11 +0530

    update require

--- index.js
+++ index.js
@@ -1,100 +1,24 @@
-// 0. number-aliquotsum (aliquotSum)
-function aliquotSum(n) {
-  var z = 0;
-  for(var i=0, I=Math.abs(n); i<I; i++)
-    if(I % i===0) z += i;
-  return z;
-};
-// 1. number-divisorcount (divisorCount)
-function divisorCount(n) {
-  // 1. count divisors 1-n
-  var z = 0;
-  for(var i=1, I=Math.abs(n); i<=I; i++)
-    if(I % i===0) z++;
-  return z;
-};
-// 2. number-divisors (divisors)
-function divisors(n) {
-  var z = [];
-  for(var i=1, I=Math.abs(n); i<=I; i++)
-    if(I % i===0) z.push(i);
-  return z;
-};
-// 3. number-divisorsum (divisorSum)
-function divisorSum(n) {
-  // 1. sum divisors 1-n
-  var z = 0;
-  for(var i=1, I=Math.abs(n); i<=I; i++)
-    if(I % i===0) z += i;
-  return z;
-};
-// 4. number-fromroman (fromRoman)
-const SYM4 = [' ', 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
-const VAL4 = [NaN, 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
-function fromRoman(str) {
-  var s = SYM4.length-1, z = 0;
-  var neg = str.search(/^\s*-/)>=0;
-  str = str.replace(/\W/g, '').toUpperCase();
-  for(var i=0, I=str.length; i<I; i+=SYM4[s].length) {
-    while(s>0 && str.substr(i, SYM4[s].length)!==SYM4[s]) s--;
-    z += VAL4[s];
-  }
-  return neg? -z:z;
-};
-// 5. number-fromscientific (fromScientific)
-function fromScientific(str) {
-  return parseFloat(toBaseLine23(str.replace(/\s+/g, '').replace(/[Xx×*]10\^?/g, 'e')));
-};
-// 6. number-is (is)
 function is(n) {
   return typeof n==='number';
-};
-// 7. number-isabundant (isAbundant)
-function isAbundant(n) {
-  var z = 0;
-  for(var i=0, I=Math.abs(n); i<I; i++)
-    if(I % i===0) z += i;
-  return z>I;
-};
-// 8. number-iscomposite (isComposite)
-function isComposite(n) {
-  return !isPrime(n);
-};
-// 9. number-isdeficient (isDeficient)
-function isDeficient(n) {
-  var z = 0;
-  for(var i=1, I=Math.abs(n); i<I; i++)
-    if(I % i===0) z += i;
-  return z<I;
-};
-// 10. number-iseven (isEven)
+}
+function isOdd(n) {
+  return Number.isInteger(n)? !!(n & 1) : undefined;
+}
 function isEven(n) {
   return !(n & 1);
-};
-// 11. number-isnatural (isNatural)
-function isNatural(n) {
-  return n>0 && Number.isInteger(n);
-};
-// 12. number-isnegative (isNegative)
+}
 function isNegative(n) {
   return n<0;
-};
-// 13. number-isodd (isOdd)
-function isOdd(n) {
-  return !!(n & 1);
-};
-// 14. number-isperfect (isPerfect)
-function isPerfect(n) {
-  var z = 0;
-  for(var i=0, I=Math.abs(n); i<I; i++)
-    if(I % i===0) z += i;
-  return z===I;
-};
-// 15. number-ispositive (isPositive)
+}
+function isNatural(n) {
+  return n>0 && Number.isInteger(n);
+}
+function isWhole(n) {
+  return n>=0 && Number.isInteger(n);
+}
 function isPositive(n) {
   return n>0;
-};
-// 16. number-isprime (isPrime)
+}
 function isPrime(n) {
   // 1. get absolute
   var N = Math.abs(n);
@@ -107,17 +31,84 @@ function isPrime(n) {
     if(N % (i-1)===0 || N % (i+1)===0) return false;
   // 5. no factor, and not 0, 1 => prime
   return N>1;
-};
-// 17. number-iswhole (isWhole)
-function isWhole(n) {
-  return n>=0 && Number.isInteger(n);
-};
-// 18. number-roundto (roundTo)
-function roundTo(val, pre=1e-12) {
-  var val = Math.round(val/pre)*pre;
-  return Math.round(val*1e+12)/1e+12;
-};
-// 19. number-toindiansystem (toIndianSystem)
+}
+function isComposite(n) {
+  return !isPrime(n);
+}
+function isAbundant(n) {
+  var z = 0;
+  for(var i=0, I=Math.abs(n); i<I; i++)
+    if(I % i===0) z += i;
+  return z>I;
+}
+function isDeficient(n) {
+  var z = 0;
+  for(var i=1, I=Math.abs(n); i<I; i++)
+    if(I % i===0) z += i;
+  return z<I;
+}
+function isPerfect(n) {
+  var z = 0;
+  for(var i=0, I=Math.abs(n); i<I; i++)
+    if(I % i===0) z += i;
+  return z===I;
+}
+function isPerfectSquare(n) {
+  return Number.isInteger(Math.sqrt(n));
+}
+function isPerfectCube(n) {
+  return Number.isInteger(Math.cbrt(n));
+}
+function significantDigits(n) {
+  return n.toExponential().replace(/e[\+\-0-9]*$/, '').replace( /^0\.?0*|\./, '').length;
+}
+const SYM = [' ', 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+const VAL = [NaN, 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+function fromRoman(str) {
+  var s = SYM.length-1, z = 0;
+  var neg = str.search(/^\s*-/)>=0;
+  str = str.replace(/\W/g, '').toUpperCase();
+  for(var i=0, I=str.length; i<I; i+=SYM[s].length) {
+    while(s>0 && str.substr(i, SYM[s].length)!==SYM[s]) s--;
+    z += VAL[s];
+  }
+  return neg? -z:z;
+}
+const FSUP = '⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾ᵝᵞᵟᶿᶥᵠᵡᴬᴮᴰᴱᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾᴿᵀᵁⱽᵂᶦᶫᶰᶸᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖʳˢᵗᵘᵛʷˣʸᶻ';
+const TSUP = '0123456789+-=()βγδθιφχABDEGHIJKLMNOPRTUVWILNUabcdefghijklmnoprstuvwxyz';
+const FSUB = '₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎ₔᵦᵧᵨᵩᵪₐₑₕᵢⱼₖₗₘₙₒₚᵣₛₜᵤᵥₓ';
+const TSUB = '0123456789+-=()əβγρφχaehijklmnoprstuvx';
+function toBaseLine(str, sup, sub) {
+  var s = 0, z = '';
+  var sup = sup||[], sub = sub||[];
+  var bgn = ['', sup[0]||'', sub[0]||''];
+  var end = ['', sup[1]||'', sub[1]||''];
+  for(var c of str) {
+    var isup = FSUP.indexOf(c), isub = FSUB.indexOf(c);
+    var sn = isup>=0? 1:(isub>=0? 2:0);
+    if(sn!==s) z += end[s]+bgn[sn];
+    z += TSUP[isup]||TSUB[isub]||c;
+    s = sn;
+  }
+  return z+end[s];
+}
+const toBaseline = toBaseLine;
+function fromScientific(str) {
+  return parseFloat(toBaseline(str.replace(/\s+/g, '').replace(/[Xx×*]10\^?/g, 'e')));
+}
+const SYM18 = '        ⁽⁾ ⁺ ⁻  ⁰¹²³⁴⁵⁶⁷⁸⁹   ⁼   ᴬᴮ ᴰᴱ ᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾ ᴿ ᵀᵁ ᵂ         ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖ ʳˢᵗᵘᵛʷˣʸᶻ     ';
+function toSuperscript(str) {
+  var z = '';
+  for(var c of str) {
+    var d = SYM18[c.charCodeAt()-32]||' ';
+    z += d===' '? c:d;
+  }
+  return z;
+}
+function toScientific(n) {
+  var e = Math.floor(Math.log10(n)), m = n*Math.pow(10, -e);
+  return m+'×10'+toSuperscript(e.toString());
+}
 function toIndianSystem(n, sep) {
   // 1. check stringified number
   var sep = sep||',', N = n.toString();
@@ -132,22 +123,7 @@ function toIndianSystem(n, sep) {
   for(var i=d+4, I=N.length; i<I; i+=2)
     z += sep+N.substr(i, 2);
   return z;
-};
-// 20. number-toroman (toRoman)
-const SYM20 = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
-const VAL20 = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
-function toRoman(n) {
-  var N = Math.abs(n), z = n<0? '-':'';
-  for(var i=SYM20.length-1; i>=0; i--)
-    while(N>=VAL20[i]) { N -= VAL20[i]; z += SYM20[i]; }
-  return z;
-};
-// 21. number-toscientific (toScientific)
-function toScientific(n) {
-  var e = Math.floor(Math.log10(n)), m = n*Math.pow(10, -e);
-  return m+'×10'+toSuperscript24(e.toString());
-};
-// 22. number-towesternsystem (toWesternSystem)
+}
 function toWesternSystem(n, sep) {
   // 1. check stringified number
   var sep = sep||',', N = n.toString();
@@ -162,57 +138,82 @@ function toWesternSystem(n, sep) {
   for(var i=I+4, I=N.length; i<I; i+=3)
     z += sep+N.substr(i, 3);
   return z;
-};
-// 23. string-tobaseline (toBaseLine23)
-const FSUP23 = '⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾ᵝᵞᵟᶿᶥᵠᵡᴬᴮᴰᴱᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾᴿᵀᵁⱽᵂᶦᶫᶰᶸᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖʳˢᵗᵘᵛʷˣʸᶻ';
-const TSUP23 = '0123456789+-=()βγδθιφχABDEGHIJKLMNOPRTUVWILNUabcdefghijklmnoprstuvwxyz';
-const FSUB23 = '₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎ₔᵦᵧᵨᵩᵪₐₑₕᵢⱼₖₗₘₙₒₚᵣₛₜᵤᵥₓ';
-const TSUB23 = '0123456789+-=()əβγρφχaehijklmnoprstuvx';
-function toBaseLine23(str, sup, sub) {
-  var s = 0, z = '';
-  var sup = sup||[], sub = sub||[];
-  var bgn = ['', sup[0]||'', sub[0]||''];
-  var end = ['', sup[1]||'', sub[1]||''];
-  for(var c of str) {
-    var isup = FSUP23.indexOf(c), isub = FSUB23.indexOf(c);
-    var sn = isup>=0? 1:(isub>=0? 2:0);
-    if(sn!==s) z += end[s]+bgn[sn];
-    z += TSUP23[isup]||TSUB23[isub]||c;
-    s = sn;
-  }
-  return z+end[s];
-};
-// 24. string-tosuperscript (toSuperscript24)
-const SYM24 = '        ⁽⁾ ⁺ ⁻  ⁰¹²³⁴⁵⁶⁷⁸⁹   ⁼   ᴬᴮ ᴰᴱ ᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾ ᴿ ᵀᵁ ᵂ         ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖ ʳˢᵗᵘᵛʷˣʸᶻ     ';
-function toSuperscript24(str) {
-  var z = '';
-  for(var c of str) {
-    var d = SYM24[c.charCodeAt()-32]||' ';
-    z += d===' '? c:d;
-  }
+}
+const SYM22 = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+const VAL22 = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+function toRoman(n) {
+  var N = Math.abs(n), z = n<0? '-':'';
+  for(var i=SYM22.length-1; i>=0; i--)
+    while(N>=VAL22[i]) { N -= VAL22[i]; z += SYM22[i]; }
   return z;
-};
-Number.aliquotSum = aliquotSum;
-Number.divisorCount = divisorCount;
-Number.divisors = divisors;
-Number.divisorSum = divisorSum;
-Number.fromRoman = fromRoman;
-Number.fromScientific = fromScientific;
+}
+function compare(n1, n2) {
+  return n1===n2? 0:(n1<n2? -1:1);
+}
+function roundTo(val, pre=1e-12) {
+  var val = Math.round(val/pre)*pre;
+  return Math.round(val*1e+12)/1e+12;
+}
+function aliquotSum(n) {
+  var z = 0;
+  for(var i=0, I=Math.abs(n); i<I; i++)
+    if(I % i===0) z += i;
+  return z;
+}
+function divisors(n) {
+  var z = [];
+  for(var i=1, I=Math.abs(n); i<=I; i++)
+    if(I % i===0) z.push(i);
+  return z;
+}
+function divisorCount(n) {
+  // 1. count divisors 1-n
+  var z = 0;
+  for(var i=1, I=Math.abs(n); i<=I; i++)
+    if(I % i===0) z++;
+  return z;
+}
+function divisorSum(n) {
+  // 1. sum divisors 1-n
+  var z = 0;
+  for(var i=1, I=Math.abs(n); i<=I; i++)
+    if(I % i===0) z += i;
+  return z;
+}
+// 1. Datatype methods
 Number.is = is;
-Number.isAbundant = isAbundant;
-Number.isComposite = isComposite;
-Number.isDeficient = isDeficient;
-Number.isEven = isEven;
-Number.isNatural = isNatural;
-Number.isNegative = isNegative;
+
+// 2. About methods
 Number.isOdd = isOdd;
-Number.isPerfect = isPerfect;
+Number.isEven = isEven;
+Number.isNegative = isNegative;
+Number.isNatural = isNatural;
+Number.isWhole = isWhole;
 Number.isPositive = isPositive;
 Number.isPrime = isPrime;
-Number.isWhole = isWhole;
-Number.round = roundTo;
-Number.toIndianSystem = toIndianSystem;
-Number.toRoman = toRoman;
+Number.isComposite = isComposite;
+Number.isAbundant = isAbundant;
+Number.isDeficient = isDeficient;
+Number.isPerfect = isPerfect;
+Number.isPerfectSquare = isPerfectSquare;
+Number.isPerfectCube = isPerfectCube;
+Number.significantDigits = significantDigits;
+
+// 3. Generate methods
+Number.fromRoman = fromRoman;
+Number.fromScientific = fromScientific;
+
+// 4. Transform methods
 Number.toScientific = toScientific;
+Number.toIndianSystem = toIndianSystem;
 Number.toWesternSystem = toWesternSystem;
+Number.toRoman = toRoman;
+
+// 5. Evaluate methods
+Number.compare = compare;
+Number.round = roundTo;
+Number.aliquotSum = aliquotSum;
+Number.divisors = divisors;
+Number.divisorCount = divisorCount;
+Number.divisorSum = divisorSum;
 module.exports = Number;
