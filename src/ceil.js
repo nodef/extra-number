fe8beded7ebd6bfaf7462d9b958c2bb5d16ce69b
Subhajit Sahu <wolfram77@gmail.com>
2020-06-26 16:43:02 +0530

    :coconut: chutney +ceil

--- src/ceil.js
+++
@@ -1,9 +0,0 @@
-/**
- * Rounds up number to specific precision.
- * @param {number} n a number
- * @param {number} pre to precision
- */
-function ceil(n, pre) {
-  return Math.ceil(n/pre)*pre;
-};
-module.exports = ceil;
---
+++ src/ceil.ts
@@ -0,0 +1,9 @@
+/**
+ * Rounds up number to specific precision.
+ * @param x a number
+ * @param pre to precision (1)
+ */
+function ceil(x: number, pre: number=1): number {
+  return Math.ceil(x/pre)*pre;
+}
+export default ceil;
