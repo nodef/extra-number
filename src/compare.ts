61d5a106aaa5aaa6aed2d52d81b3ab81f86c6bf3
Subhajit Sahu <wolfram77@gmail.com>
2020-06-26 16:19:50 +0530

    :coconut: chutney +compare

---
+++ src/compare.ts
@@ -0,0 +1,10 @@
+/**
+ * Compares two values.
+ * @param x a number
+ * @param y another number
+ * @returns x<y: -1, x=y: 0, x>y: 1
+ */
+function compare(x: number, y: number): number {
+  return x<y? -1 : (x>y? 1 : 0);
+}
+export default compare;
