41d360ca839ef2c8a8ba50fe710dbe4fea418106
Subhajit Sahu <wolfram77@gmail.com>
2020-06-26 16:44:17 +0530

    :coconut: chutney +floor

--- src/floor.js
+++
@@ -1,9 +0,0 @@
-/**
- * Rounds down number to specific precision.
- * @param {number} n a number
- * @param {number} pre to precision
- */
-function floor(n, pre) {
-  return Math.floor(n/pre)*pre;
-};
-module.exports = floor;
