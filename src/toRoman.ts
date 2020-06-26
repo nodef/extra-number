75adbfa36914a1e511ab4e9dbcca24bcb45bf4cc
Subhajit Sahu <wolfram77@gmail.com>
2020-06-26 16:38:40 +0530

    :coconut: chutney +toRoman

--- src/toRoman.ts
+++ src/toRoman.ts
@@ -3,13 +3,13 @@ const VAL = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 
 /**
  * Converts number to roman numerals.
- * @param {number} n a number
- * @returns {string} eg. 95 -> XCV
+ * @param x a number
+ * @returns eg. 95 -> XCV
  */
-function toRoman(n) {
-  var N = Math.abs(n), z = n<0? '-':'';
+function toRoman(x: number): string {
+  var a = x<0? '-':'', x = Math.abs(x);
   for(var i=SYM.length-1; i>=0; i--)
-    while(N>=VAL[i]) { N -= VAL[i]; z += SYM[i]; }
-  return z;
-};
-module.exports = toRoman;
+    while(x>=VAL[i]) { x -= VAL[i]; a += SYM[i]; }
+  return a;
+}
+export default toRoman;
