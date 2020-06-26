2f96deea78cc64e219874e731e2661e93d478229
Subhajit Sahu <wolfram77@gmail.com>
2020-06-26 16:50:53 +0530

    :coconut: chutney +lerp

--- src/ceil.ts
+++ src/ceil.ts
@@ -1,7 +1,7 @@
 /**
  * Rounds up number to specific precision.
  * @param x a number
- * @param pre to precision (1)
+ * @param pre to precision [1]
  */
 function ceil(x: number, pre: number=1): number {
   return Math.ceil(x/pre)*pre;
--- src/round.ts
+++ src/round.ts
@@ -1,7 +1,7 @@
 /**
  * Rounds number to specific precision.
  * @param x a number
- * @param pre to precision (1e-12)
+ * @param pre to precision [1e-12]
  */
 function round(x: number, pre: number=1e-12): number {
   var x = Math.round(x/pre)*pre;
