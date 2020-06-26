7bf9620747a1e0730b72b10666fd2eb5b0807e30
Subhajit Sahu <wolfram77@gmail.com>
2020-06-26 16:46:19 +0530

    :coconut: chutney +clamp

---
+++ src/clamp.ts
@@ -0,0 +1,10 @@
+/**
+ * Limits number within given range.
+ * @param x a number
+ * @param v start of target range (0)
+ * @param V end of target range (1)
+ */
+function clamp(x: number, v: number=0, V: number=1): number {
+  return Math.min(Math.max(x, v), V);
+}
+export default clamp;
