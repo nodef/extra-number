d357d7c862b0b07c5397791da7dbf95ab0a97c05
Subhajit Sahu <wolfram77@gmail.com>
2022-08-07 22:55:35 +0530

    :mango: correct existing functions

--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,23 @@
 // -----
 
 /**
- * Check if value is number.
+ * Check if value is a number.
  * @param v a value
+ * @returns is number?
  */
 export function is(v: any): v is number {
-  return typeof v==='number';
-}
-
-
-/**
- * Check if number is prime.
- * @param x a number
- */
-export function isPrime(x: number): boolean {
-  var x = Math.abs(x);
-  // 2, 3 are prime
-  if (x<=3) return x>1;
-  // Multiples of 2, 3 not prime
-  if (x % 2===0 || x % 3===0) return false;
-  // Factor of 6k-1 or 6k+1 => not prime
-  for (var i=6, I=Math.sqrt(x)+1; i<=I; i+=6)
-    if (x % (i-1)===0 || x % (i+1)===0) return false;
-  return true;
+  return typeof v==="number";
 }
 
 
 /**
  * Count the number of significant digits in a number.
  * @param x a number
+ * @returns |m - '.'| | x = m × 10ᵉ; m = mantissa, e = exponent
  */
 export function significantDigits(x: number): number {
   var a = x.toExponential();
-  return a.replace(/e[\+\-0-9]*$/, '').replace( /^0\.?0*|\./, '').length;
+  return a.replace(/e[\+\-0-9]*$/, "").replace( /^0\.?0*|\./, "").length;
 }
 
 
@@ -58,7 +43,7 @@ export function compare(x: number, y: number): number {
 
 
 
-// RANGE
+// ROUND
 // -----
 
 /**
@@ -72,17 +57,6 @@ export function floor(x: number, pre: number=1): number {
 
 
 /**
- * Round a number to specific precision.
- * @param x a number
- * @param pre to precision [1e-12]
- */
-export function round(x: number, pre: number=1e-12): number {
-  var x = Math.round(x/pre)*pre;
-  return  Math.round(x*1e+12)/1e+12;
-}
-
-
-/**
  * Round up a number to specific precision.
  * @param x a number
  * @param pre to precision [1]
@@ -92,11 +66,14 @@ export function ceil(x: number, pre: number=1): number {
 }
 
 
-export {constrain}          from "extra-math";
-export {constrain as clamp} from "extra-math";
-export {map}  from "extra-math"
-export {norm} from "extra-math"
-export {lerp} from "extra-math"
+/**
+ * Round a number to specific precision.
+ * @param x a number
+ * @param pre to precision [1]
+ */
+export function round(x: number, pre: number=1): number {
+  return Math.round(x/pre)*pre;
+}
 
 
 
@@ -120,12 +97,31 @@ export function properDivisors(x: number): number[] {
 /**
  * Sum all proper divisors of a number.
  * @param x a number
+ * @returns Σdᵢ | dᵢ is a divisor of x and ≠x
  */
 export function aliquotSum(x: number): number {
   var x = Math.abs(x), a = 0;
   for (var i=0; i<x; i++)
     if (x % i===0) a += i;
   return a;
+}
+
+
+/**
+ * Check if number is prime.
+ * @param x a number
+ * @returns is divisible by 1 and itself only?
+ */
+export function isPrime(x: number): boolean {
+  var x = Math.abs(x);
+  // 1. 2, 3 are prime
+  if (x<=3) return x>1;
+  // 2. Multiples of 2, 3 not prime
+  if (x % 2===0 || x % 3===0) return false;
+  // 3. Factor of 6k-1 or 6k+1 => not prime
+  for (var i=6, I=Math.sqrt(x)+1; i<=I; i+=6)
+    if (x % (i-1)===0 || x % (i+1)===0) return false;
+  return true;
 }
 
 
