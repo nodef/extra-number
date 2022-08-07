d800c920e52f17f4e5c61b0ab18df1c7071f338b
Subhajit Sahu <wolfram77@gmail.com>
2022-08-08 01:18:14 +0530

    :mango: update test imports, example1()

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,48 +1,67 @@
-import * as math from "../src";
+import * as number   from "../src";
+import {is}          from "../src";
+import {significantDigits} from "../src";
+import {compare}     from "../src";
+import {floor}       from "../src";
+import {ceil}        from "../src";
+import {round}       from "../src";
+import {floorDiv}    from "../src";
+import {ceilDiv}     from "../src";
+import {roundDiv}    from "../src";
 import {rem}         from "../src";
 import {mod}         from "../src";
 import {modp}        from "../src";
+import {constrain}   from "../src";
+import {normalize}   from "../src";
+import {remap}       from "../src";
+import {lerp}        from "../src";
+import {isPow}       from "../src";
+import {prevPow}     from "../src";
+import {nextPow}     from "../src";
+import {root}        from "../src";
+import {log}         from "../src";
+import {properDivisors} from "../src";
+import {aliquotSum}     from "../src";
+import {isPrime}        from "../src";
 import {gcd}         from "../src";
 import {lcm}         from "../src";
 import {factorial}   from "../src";
 import {binomial}    from "../src";
 import {multinomial} from "../src";
-import {constrain}   from "../src";
-import {normalize}   from "../src";
-import {remap}       from "../src";
-import {lerp}        from "../src";
-import {root}        from "../src";
-import {log}         from "../src";
 import {degrees}     from "../src";
 import {radians}     from "../src";
 import {sum}         from "../src";
 import {product}     from "../src";
-import {mean}        from "../src";
 import {median}      from "../src";
 import {modes}       from "../src";
 import {range}       from "../src";
 import {variance}    from "../src";
+import {arithmeticMean} from "../src";
+import {geometricMean}  from "../src";
+import {harmonicMean}   from "../src";
+import {quadriaticMean} from "../src";
+import {cubicMean}      from "../src";
 
 
 
 
 // 1. Basic tests.
 test("example1", () => {
-  var a = math.sum(1, 2, 3, 4);
-  expect(a).toBe(10);
-  // → 10
+  var b = number.isPrime(53);
+  expect(b).toBe(true);
+  // → true
 
-  var a = math.median(1, 7, 8);
-  expect(a).toBe(7);
-  // → 7
+  var x = number.properDivisors(6);
+  expect(x).toStrictEqual([1, 2, 3]);
+  // → [1, 2, 3]
 
-  var a = math.variance(1, 2, 3, 4);
-  expect(a).toBe(1.25);
-  // → 1.25
+  var a = number.round(9.1357, 0.05);
+  expect(x).toBe(9.15);
+  // → 9.15
 
-  var a = math.lcm(2, 3, 4);
-  expect(a).toBe(12);
-  // → 12
+  var a = number.significantDigits(0.0034);
+  expect(a).toBe(2);
+  // → 2
 });
 
 
@@ -75,67 +94,6 @@ test("modp", () => {
   expect(a).toBe(9);
   var a = modp(1, -10);
   expect(a).toBe(1);
-});
-
-
-test("gcd", () => {
-  var a = gcd(6, 15);
-  expect(a).toBe(3);
-  var a = gcd(6, 15, 21);
-  expect(a).toBe(3);
-  var a = gcd(6, 15, 20);
-  expect(a).toBe(1);
-  var a = gcd();  // sp
-  expect(a).toBe(1);  // sp
-});
-
-
-test("lcm", () => {
-  var a = lcm(2, 3);
-  expect(a).toBe(6);
-  var a = lcm(2, 3, 4);
-  expect(a).toBe(12);
-  var a = lcm(2, 3, 4, 5);
-  expect(a).toBe(60);
-  var a = lcm();  // sp
-  expect(a).toBe(1);  // sp
-});
-
-
-test("factorial", () => {
-  var a = factorial(5);
-  expect(a).toBe(120);
-  var a = factorial(6);
-  expect(a).toBe(720);
-  var a = factorial(7);
-  expect(a).toBe(5040);
-  var a = factorial(-1);  // sp
-  expect(a).toBe(0);  // sp
-});
-// - https://en.wikipedia.org/wiki/Factorial
-
-
-test("binomial", () => {
-  var a = binomial(4, 1);
-  expect(a).toBe(4);
-  var a = binomial(4, 2);
-  expect(a).toBe(6);
-  var a = binomial(4, 3);
-  expect(a).toBe(4);
-  var a = binomial(4, -1);  // sp
-  expect(a).toBe(0);  // sp
-  var a = binomial(-4, 1);  // sp
-  expect(a).toBe(-4);  // sp
-});
-
-
-test("multinomial", () => {
-  var a = multinomial(1, 2);
-  expect(a).toBe(3);
-  var a = multinomial(1, 2, 3);
-  expect(a).toBe(60);
-  var a = multinomial(1, 2, 3, 4);
-  expect(a).toBe(12600);
 });
 
 
@@ -201,51 +159,64 @@ test("log", () => {
 // - https://en.wikipedia.org/wiki/Logarithm
 
 
-// test("gamma", () => {
-//   var a = gamma(1/2);
-//   expect(a).toBe(Math.sqrt(Math.PI));
-//   var a = gamma(3/2);
-//   expect(a).toBe((1/2)*Math.sqrt(Math.PI));
-//   var a = gamma(-3/2);
-//   expect(a).toBe((4/3)*Math.sqrt(Math.PI));
-//   var a = gamma(-5/2);
-//   expect(a).toBe(-(8/15)*Math.sqrt(Math.PI));
-// });
-// - https://en.wikipedia.org/wiki/Gamma_function
-
-
-// test("lgamma", () => {
-//   var a = lgamma(2);
-//   expect(a).toBe(0);
-//   var a = lgamma(3);
-//   expect(a).toBe(0.6931472);
-//   var a = lgamma(5);
-//   expect(a).toBe(3.178054);
-// });
-// - https://www.geeksforgeeks.org/compute-the-natural-logarithm-of-the-absolute-value-of-gamma-function-in-r-programming-lgamma-function/
-
-
-test("erf", () => {
-  var a = erf(0);
-  expect(a).toBe(9.999999717180685e-10);
-  var a = erf(0.1);
-  expect(a).toBe(0.11246296562219549);
-  var a = erf(1);
-  expect(a).toBe(0.8427006897475899);
-  var a = erf(-1);  // sp
-  expect(a).toBe(-0.8427006897475899);  // sp
+test("gcd", () => {
+  var a = gcd(6, 15);
+  expect(a).toBe(3);
+  var a = gcd(6, 15, 21);
+  expect(a).toBe(3);
+  var a = gcd(6, 15, 20);
+  expect(a).toBe(1);
+  var a = gcd();  // sp
+  expect(a).toBe(1);  // sp
 });
 
 
-test("erfc", () => {
-  var a = erfc(0);
-  expect(a).toBe(0.9999999999999999);
-  var a = erfc(0.1);
-  expect(a).toBe(0.887537083981715);
-  var a = erfc(1);
-  expect(a).toBe(0.1572992070502851);
-  var a = erfc(-1);  // sp
-  expect(a).toBe(2-0.1572992070502851);  // sp
+test("lcm", () => {
+  var a = lcm(2, 3);
+  expect(a).toBe(6);
+  var a = lcm(2, 3, 4);
+  expect(a).toBe(12);
+  var a = lcm(2, 3, 4, 5);
+  expect(a).toBe(60);
+  var a = lcm();  // sp
+  expect(a).toBe(1);  // sp
+});
+
+
+test("factorial", () => {
+  var a = factorial(5);
+  expect(a).toBe(120);
+  var a = factorial(6);
+  expect(a).toBe(720);
+  var a = factorial(7);
+  expect(a).toBe(5040);
+  var a = factorial(-1);  // sp
+  expect(a).toBe(0);  // sp
+});
+// - https://en.wikipedia.org/wiki/Factorial
+
+
+test("binomial", () => {
+  var a = binomial(4, 1);
+  expect(a).toBe(4);
+  var a = binomial(4, 2);
+  expect(a).toBe(6);
+  var a = binomial(4, 3);
+  expect(a).toBe(4);
+  var a = binomial(4, -1);  // sp
+  expect(a).toBe(0);  // sp
+  var a = binomial(-4, 1);  // sp
+  expect(a).toBe(-4);  // sp
+});
+
+
+test("multinomial", () => {
+  var a = multinomial(1, 2);
+  expect(a).toBe(3);
+  var a = multinomial(1, 2, 3);
+  expect(a).toBe(60);
+  var a = multinomial(1, 2, 3, 4);
+  expect(a).toBe(12600);
 });
 
 
@@ -265,26 +236,6 @@ test("radians", () => {
 });
 
 
-test("magnitude", () => {
-  var a = magnitude([5, 9]);
-  expect(a).toBe(Math.sqrt(106));
-  var a = magnitude([10, 2, -1]);
-  expect(a).toBe(Math.sqrt(105));
-});
-// - https://www.cuemath.com/geometry/distance-between-two-points/
-// - https://www.calculatorsoup.com/calculators/geometry-solids/distance-two-points.php
-
-
-test("distance", () => {
-  var a = distance([2, -6], [7, 3]);
-  expect(a).toBe(Math.sqrt(106));
-  var a = distance([7, 4, 3], [17, 6, 2]);
-  expect(a).toBe(Math.sqrt(105));
-});
-// - https://www.cuemath.com/geometry/distance-between-two-points/
-// - https://www.calculatorsoup.com/calculators/geometry-solids/distance-two-points.php
-
-
 test("sum", () => {
   var a = sum(1, 2);
   expect(a).toBe(3);
@@ -302,16 +253,6 @@ test("product", () => {
   expect(a).toBe(6);
   var a = product(1, 2, 3, 4);
   expect(a).toBe(24);
-});
-
-
-test("mean", () => {
-  var a = mean(1, 2);
-  expect(a).toBe(1.5);
-  var a = mean(1, 2, 3);
-  expect(a).toBe(2);
-  var a = mean(1, 2, 3, 4);
-  expect(a).toBe(2.5);
 });
 
 
@@ -352,4 +293,14 @@ test("variance", () => {
   expect(a).toBe(0.6666666666666666);
   var a = variance(1, 2, 3, 4);
   expect(a).toBe(1.25);
+});
+
+
+test("arithmeticMean", () => {
+  var a = arithmeticMean(1, 2);
+  expect(a).toBe(1.5);
+  var a = arithmeticMean(1, 2, 3);
+  expect(a).toBe(2);
+  var a = arithmeticMean(1, 2, 3, 4);
+  expect(a).toBe(2.5);
 });
